41ae1f1b05bdab292749659b26dae99e84eefb0b
JhnRavelo <johnravelo135@gmail.com>
2024-04-18 16:09:32 +0300

    imporvement: specify error

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,7 +3,6 @@ const bcrypt = require("bcrypt");
 require("dotenv").config();
 const sequelize = require("sequelize");
 const { Op } = require("sequelize");
-const contByMonth = require("../utils/countByMonth");
 
 var date = new Date();
 var day = date.getDate();
@@ -14,62 +13,65 @@ const userRegistration = async (req, res) => {
   try {
     const { name, email, phone, password, typeUser } = await req.body;
     var userName;
-    if (email) {
-      userName = await users.findOne({
-        where: {
-          email: email,
-        },
+
+    if (!name || !email || !phone || !password || !typeUser)
+      return res.json({ success: false });
+    userName = await users.findOne({
+      where: {
+        email: email,
+      },
+    });
+
+    if (userName)
+      return res.json({ success: false, message: `L'utilisateur existe déjà` });
+    const userRegister = await users.create({
+      name,
+      email,
+      phone,
+      password: await bcrypt.hash(password, 10),
+      type: typeUser,
+    });
+    const id = userRegister.ID_user,
+      role = userRegister.role,
+      refreshToken = users.prototype.generateRefreshToken(id),
+      accessToken = users.prototype.generateToken(id, role);
+
+    userRegister.refreshToken = refreshToken;
+    const result = await userRegister.save();
+
+    if (!result)
+      return res.json({
+        success: false,
+        message: "Utilisateur non enregistré",
+      });
+
+    if (userRegister.role == process.env.PRIME3) {
+      await sessions.create({
+        userId: userRegister.ID_user,
+        day,
+        month,
+        year,
+      });
+      await logs.create({
+        userId: userRegister.ID_user,
       });
     }
-
-    if (userName) {
-      res.json(`L'utilisateur existe déjà`);
-    } else if (name && email && !userName && phone && password && typeUser) {
-      const userRegister = await users.create({
-        name,
-        email,
-        phone,
-        password: await bcrypt.hash(password, 10),
-        type: typeUser,
-      });
-
-      const id = userRegister.ID_user,
-        role = userRegister.role,
-        refreshToken = users.prototype.generateRefreshToken(id),
-        accessToken = users.prototype.generateToken(id, role);
-
-      userRegister.refreshToken = refreshToken;
-
-      const result = await userRegister.save();
-
-      if (!result) return res.json("Utilisateur non enregistré");
-
-      if (userRegister.role == process.env.PRIME3) {
-        await sessions.create({
-          userId: userRegister.ID_user,
-          day,
-          month,
-          year,
-        });
-
-        await logs.create({
-          userId: userRegister.ID_user,
-        });
-      }
-
-      res.cookie("jwt", refreshToken, {
-        maxAge: 24 * 60 * 60 * 1000,
-        httpOnly: true,
-        // sameSite: "None",
-        secure: true,
-      });
-
-      res.json({ role, accessToken });
-    } else {
-      res.json("Suivant");
-    }
+    res.cookie("jwt", refreshToken, {
+      maxAge: 24 * 60 * 60 * 1000,
+      httpOnly: true,
+    });
+    res.json({
+      role,
+      accessToken,
+      success: true,
+      name: userRegister.name,
+      email: userRegister.email,
+      phone: userRegister.phone,
+      avatar: userRegister.avatar,
+      id: userRegister.ID_user,
+    });
   } catch (error) {
-    console.log(error);
+    console.log("ERROR USER REGISTER", error);
   }
 };
 
@@ -117,8 +119,6 @@ const userLogin = async (req, res) => {
         newRefreshToken = "";
         res.clearCookie("jwt", {
           httpOnly: true,
-          // sameSite: "None",
-          secure: true,
         });
       }
     }
@@ -129,8 +129,6 @@ const userLogin = async (req, res) => {
 
     res.cookie("jwt", newRefreshToken, {
       httpOnly: true,
-      // sameSite: "None",
-      secure: true,
       maxAge: 24 * 60 * 60 * 1000,
     });
 
@@ -143,35 +141,18 @@ const userLogin = async (req, res) => {
       });
     }
 
-    res.json({ role, accessToken, success: true });
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-const userRead = async (req, res) => {
-  try {
-    const cookie = await req.cookies;
-    if (!cookie?.jwt) return res.sendStatus(401);
-    const refreshToken = cookie.jwt;
-
-    const user = await users.findOne({
-      where: {
-        refreshToken: refreshToken,
-      },
-    });
-
-    if (!user) return res.sendStatus(401);
-
     res.json({
-      id: user.ID_user,
-      name: user.name,
-      email: user.email,
-      phone: user.phone,
-      avatar: user.avatar,
+      role,
+      accessToken,
+      success: true,
+      name: userName.name,
+      email: userName.email,
+      phone: userName.email,
+      avatar: userName.avatar,
+      id: userName.ID_user,
     });
   } catch (error) {
-    console.log(error);
+    console.log("ERROR USER LOGIN", error);
   }
 };
 
@@ -180,9 +161,7 @@ const userLogout = async (req, res) => {
     const cookie = req.cookies;
 
     if (!cookie?.jwt) return res.sendStatus(204);
-
     const refreshToken = cookie.jwt;
-
     const user = await users.findOne({
       where: {
         refreshToken: refreshToken,
@@ -192,24 +171,18 @@ const userLogout = async (req, res) => {
     if (!user) {
       res.clearCookie("jwt", {
         httpOnly: true,
-        // sameSite: "None",
-        secure: true,
       });
       return res.sendStatus(204);
     }
     console.log("vider");
     user.refreshToken = "";
     await user.save();
-
     res.clearCookie("jwt", {
       httpOnly: true,
-      // sameSite: "None",
-      secure: true,
     });
-
     return res.json("SUCCESS");
   } catch (error) {
-    console.log(error);
+    console.log("ERROR USER LOGOUT", error);
   }
 };
 
@@ -239,13 +212,11 @@ const addUser = async (req, res) => {
       if (type) {
         userAdd.type = type;
       }
+
       if (role) {
         userAdd.role = role;
       }
-
       const result = await userAdd.save();
-
-      console.log(result);
 
       if (result) {
         if (userAdd.role == process.env.PRIME3) {
@@ -255,7 +226,7 @@ const addUser = async (req, res) => {
       }
     }
   } catch (error) {
-    console.log(error);
+    console.log("ERROR ADD USER", error);
   }
 };
 
@@ -289,14 +260,13 @@ const uploadUserImage = async (req, res) => {
     }
 
     if (avatar) userUpload.avatar = avatar;
-
     const result = await userUpload.save();
 
     if (result) {
       res.json("Upload product");
     }
   } catch (error) {
-    console.log(error);
+    console.log("ERROR UPLOAD USER IMAGE", error);
   }
 };
 
@@ -307,36 +277,21 @@ const getUsers = async (req, res) => {
         role: process.env.PRIME3,
       },
     });
-
     const filterResult = result.map((item) => {
-      if (item.refreshToken) {
-        return {
-          ID_user: item.ID_user,
-          name: item.name,
-          email: item.email,
-          phone: item.phone,
-          avatar: item.avatar,
-          createdAt: item.createdAt,
-          connected: true,
-          type: item.type,
-        };
-      } else {
-        return {
-          ID_user: item.ID_user,
-          name: item.name,
-          email: item.email,
-          phone: item.phone,
-          avatar: item.avatar,
-          createdAt: item.createdAt,
-          connected: false,
-          type: item.type,
-        };
-      }
+      return {
+        ID_user: item.ID_user,
+        name: item.name,
+        email: item.email,
+        phone: item.phone,
+        avatar: item.avatar,
+        createdAt: item.createdAt,
+        connected: item?.refreshToken ? true : false,
+        type: item.type,
+      };
     });
-
     res.json(filterResult);
   } catch (error) {
-    console.log(error);
+    console.log("ERROR GET USER", error);
   }
 };
 
@@ -345,23 +300,19 @@ const updateUser = async (req, res) => {
     const { name, updateEmail, phone, updatePassword, type, id, role } =
       await req.body;
 
-    if (!id) return res.json("Sans id");
-
+    if (!id) return res.sendStatus(403);
     const user = await users.findOne({ where: { ID_user: id } });
 
-    if (!user) return res.json("Sans user");
-
+    if (!user) return res.sendStatus(403);
     if (name) user.name = name;
     if (updateEmail) user.email = updateEmail;
     if (phone) user.phone = phone;
     if (type) {
       user.type = type;
     }
-
     if (role) {
       user.role = role;
     }
-
     if (updatePassword) {
       user.password = await bcrypt.hash(updatePassword, 10);
     }
@@ -369,21 +320,22 @@ const updateUser = async (req, res) => {
 
     if (result) return res.json("Utilisateur modifié");
   } catch (error) {
-    console.log(error);
+    console.log("ERROR UPDATE USER", error);
   }
 };
 
 const updateProfile = async (req, res) => {
   try {
     const { name, id } = await req.body;
+    if (!name || !id) return res.sendStatus(401);
     const user = await users.findOne({ where: { ID_user: id } });
-    if (name) user.name = name;
+    user.name = name;
 
     const result = await user.save();
 
     if (result) return res.json("Utilisateur modifié");
   } catch (error) {
-    console.log(error);
+    console.log("ERROR UPDATE PROFILE", error);
   }
 };
 
@@ -400,7 +352,7 @@ const deleteUser = async (req, res) => {
       if (result) return res.json("supprimé");
     } else res.json("non supprimé");
   } catch (error) {
-    console.log(error);
+    console.log("ERROR DELETE USER", error);
   }
 };
 
@@ -413,39 +365,26 @@ const getCommercials = async (req, res) => {
     });
 
     const filterResult = result.map((item) => {
-      if (item.refreshToken) {
-        return {
-          ID_user: item.ID_user,
-          name: item.name,
-          email: item.email,
-          phone: item.phone,
-          avatar: item.avatar,
-          createdAt: item.createdAt,
-          connected: true,
-        };
-      } else {
-        return {
-          ID_user: item.ID_user,
-          name: item.name,
-          email: item.email,
-          phone: item.phone,
-          avatar: item.avatar,
-          createdAt: item.createdAt,
-          connected: false,
-        };
-      }
+      return {
+        ID_user: item.ID_user,
+        name: item.name,
+        email: item.email,
+        phone: item.phone,
+        avatar: item.avatar,
+        createdAt: item.createdAt,
+        connected: item?.refreshToken ? true : false,
+      };
     });
 
     return res.json(filterResult);
   } catch (error) {
-    console.log(error);
+    console.log("ERROR GET COMMERCIALS", error);
   }
 };
 
 const avatarUpdateUser = async (req, res) => {
   try {
     const { avatar } = req.body;
-
     const user = await users.findOne({
       where: {
         ID_user: req.user,
@@ -458,77 +397,61 @@ const avatarUpdateUser = async (req, res) => {
       return res.json("Avatar");
     } else return res.json("Non");
   } catch (error) {
-    console.log(error);
+    console.log("ERROR AVATAR UPDATE USER", error);
   }
 };
 
 const nbrUser = async (req, res) => {
   try {
-    const { year } = await req.body;
+    const { year } = req.body;
 
     const countUser = await users.findAll({
       where: {
         role: process.env.PRIME3,
       },
-      attributes: [
-        [sequelize.literal("YEAR(createdAt)"), "year"],
-        [sequelize.literal("MONTH(createdAt)"), "month"],
-      ],
+      attributes: [[sequelize.literal("YEAR(createdAt)"), "year"]],
     });
 
-    const UserByYear = countUser.filter((result) => {
+    const filterUserByYear = countUser.filter((result) => {
       return result.dataValues.year == year;
     });
 
     const countUserByYear = {
-      userCount: UserByYear.length + 1,
+      userCount: filterUserByYear.length,
     };
 
-    const userCountsByMonth = {};
-
-    UserByYear.forEach((user) => {
-      const month = user.getDataValue("month");
-
-      const key = `${month}`;
-      if (userCountsByMonth[key]) {
-        userCountsByMonth[key]++;
-      } else {
-        userCountsByMonth[key] = 1;
-      }
+    const countUserByMonth = await users.findAll({
+      where: {
+        role: process.env.PRIME3,
+      },
+      attributes: [
+        [sequelize.literal("MONTH(createdAt)"), "month"],
+        [sequelize.literal("YEAR(createdAt)"), "year"],
+        [sequelize.fn("COUNT", sequelize.col("ID_user")), "count"],
+      ],
+      group: ["month"],
+      order: sequelize.literal("month"),
     });
 
-    // Étape 3 : Générez un tableau d'objets avec les résultats.
-    const userCountsArray = [];
-
-    for (const key in userCountsByMonth) {
-      if (userCountsByMonth.hasOwnProperty(key)) {
-        userCountsArray.push({
-          month: key,
-          count: userCountsByMonth[key],
-        });
-      }
-    }
-
-    // Triez le tableau par mois (facultatif)
-    userCountsArray.sort((a, b) => {
-      return a.month - b.month;
+    const countByMonthByYear = countUserByMonth.filter((result) => {
+      return result.dataValues.year == year;
     });
 
-    const countByMonthByYear = userCountsArray;
-
-    const userVisit = await sessions.findAll({
+    const userVisitByMonth = await sessions.findAll({
       where: {
         year: year,
       },
-      attributes: ["month"],
+      attributes: [
+        "month",
+        [sequelize.fn("COUNT", sequelize.col("userId")), "count"],
+      ],
+      group: ["month"],
+      order: ["month"],
     });
 
-    const userVisitByMonth = contByMonth(userVisit, "month");
-
     res.json({ countUserByYear, countByMonthByYear, userVisitByMonth });
-    console.log(countUserByYear);
   } catch (error) {
-    console.log(error);
+    console.log("ERROR NBR USER", error);
   }
 };
 
@@ -536,7 +459,6 @@ module.exports = {
   userRegistration,
   userLogin,
   userLogout,
-  userRead,
   addUser,
   getUsers,
   updateUser,
